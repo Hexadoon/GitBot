bebdcc39da7f55570d53b9760b45c5206bfc5dcb
Bill Ni <bill.8ni@gmail.com>
2020-04-04 02:51:43 -0400

    added issue checking functionality

--- index.js
+++ index.js
@@ -11,31 +11,47 @@ const cnxn = mysql.createConnection({
   	database: "users"
 });
 
-cnxn.connect();
+const errmsg = "an error has occurred, please contact admin."
 
-var getGit = (discord_user) => {
-	cnxn.query(("SELECT github_user FROM users WHERE discord_user = '" + discord_user + "'"), function (err, result, fields) {
-		if (err) throw err;
-		return result[0].github_user;
-	});
-}
+cnxn.connect();
 
 client.on('message', msg => {
 	if (!msg.author.bot){
 		if(msg.content.toLowerCase().indexOf('!issues') >= 0){
-			var github_user = getGit(msg.author.username);
-			fetch('https://api.github.com/users/hexadoon/repos')
-				.then(resp => resp.json())
-				.then(function(data){
-					for(var i = 0; i < data.length; i++){
-						console.log(data[i].subscribers_url);
-					}
-				})
+			cnxn.query("SELECT github_user FROM users WHERE discord_user = '" + msg.author.tag + "'", (err, request) => {
+					var github_user = JSON.parse(JSON.stringify(request))[0].github_user;
+					fetch('https://api.github.com/users/hexadoon/repos')
+						.then(repos => repos.json())
+						.then(function(repos_json){
+							for(var i = 0; i < repos_json.length; i++){
+								var issues_url = repos_json[i].issues_url.substring(0, repos_json[i].issues_url.indexOf("{"));
+								fetch(issues_url)
+									.then(issues => issues.json())
+									.then(function(issues_json){
+										for(var j = 0; j < issues_json.length; j++){
+											for(var k = 0; k < issues_json[j].assignees.length; k++){
+												if(issues_json[j].assignees[k].login === github_user){
+													msg.reply(issues_json[j].html_url);
+												}
+											}
+										}
+									})
+									.catch(function(error){
+										console.log(error);
+										msg.reply(errmsg);
+									});
+							}
+						})
+						.catch(function(error){
+							console.log(error);
+							msg.reply(errmsg);
+						});
+				});
 		}else{
 			const mtns = msg.mentions.users.array();
 			for (var k in mtns){
-				if (mtns[k].bot){
-					msg.reply('Use `!howto` for valid commands');
+				if (mtns[k].bot && mtns[k].username == "Hexagoon"){
+					msg.reply('use `!howto` for valid commands');
 					break;
 				}
 			}
